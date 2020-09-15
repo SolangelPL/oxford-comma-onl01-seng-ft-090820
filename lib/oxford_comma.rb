2e2a61d617399e26af04b720375cc385fe6cc3b8
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-15 19:33:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 def oxford_comma(array)
   (array).join
-  end
-  (array).split( and )
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2 
+    return array.joint('and')
+  else   
 end
