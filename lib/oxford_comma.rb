1127fb78293669a41096e2827834de4fb7c1ed6c
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-15 19:36:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
   (array).join
-  
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2 
+    return array.joint('and')
+  else   
+    last_item = array[-1]
+    last_item.insert(0, 'and')
+    return array.join(',')
 end
