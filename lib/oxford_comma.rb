6bf69daaa740322407876752332d495df7696d4d
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-15 19:39:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.length == 1 
     return array[0]
   elsif array.length == 2 
-    return array.join('and')
+    return array.join(' and ')
   else
     last_item = array[-1]
     last_item.insert(0, 'and')
