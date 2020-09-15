7eaeb7d7a994e369030435bef200a6da3d15d38a
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-15 19:45:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  (array).join
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2 
+    return array.join(' and ')
+  else
+    last_item = array[-1]
+    last_item.insert(0, 'and ')
+    return array.join(', ')
+  end
 end
